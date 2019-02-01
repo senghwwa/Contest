36ac990f6057c0e0a641845e7d3e90631d2f662e
senghwwa <senghwabeh@hotmail.com>
2019-02-01 17:22:55 +0800

    Initial Commit

---
+++ Contest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Contest
+//
+//  Created by Seng Hwwa on 01/02/2019.
+//  Copyright © 2019 senghwabeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
